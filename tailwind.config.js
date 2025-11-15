dfcbed04c72e0a48673e8156a9b6428691c07987
Zhafirahamalia24-lad <zhafirahamalia24@student.esaunggul.ac.id>
2025-11-15 23:15:00 +0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,jsx}',
-    './src/components/**/*.{js,jsx}',
-  ],
-  theme: { extend: {} },
-  plugins: [],
-}
